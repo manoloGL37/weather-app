2c26e82db1346c706fdaeeda6dc79c709f660263
Manolo G <manolo.gongora1@gmail.com>
2024-01-11 18:41:14 +0100

    init

---
+++ src/app/components/main/main-app/main-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-app.component.html',
+  styleUrl: './main-app.component.css'
+})
+export class MainAppComponent {
+
+}
---
+++ src/app/components/main/right-panel/right-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './right-panel.component.html',
+  styleUrl: './right-panel.component.css'
+})
+export class RightPanelComponent {
+
+}
